4bb5205c789dccb3af3346e4fb8d90f10e64efea
Gonza <gonzalojay05@gmail.com>
2026-03-25 02:58:53 +0000

    Everything needed for initial web development setup with django and react

---
+++ houserParty/house_party/frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./templates/frontend/index.html",
+    }),
+  ],
+};
